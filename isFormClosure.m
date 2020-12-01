26b9a1f9c1bd94e731898c87bd79d96e91a9fe20
waihan <wh.kankan13@gmail.com>
2020-12-01 18:02:09 +0630

    FIX BUG: fix error for rank less than 3 and add some help messages

--- isFormClosure.m
+++ isFormClosure.m
@@ -1,4 +1,16 @@
 function k = isFormClosure(contact)
+    % HELP SESSION :
+    % _______________________________
+    % for object not in form closure.    
+    % contact = [1.5 1 225; -1.5 1 315; -1.5 -1 45; 1.5 -1 154];
+    % k = isFormClosure(contact);
+    % OUTPUT: The object is not in form closure; Error using
+    % isFormClosure(line 38).
+    % _______________________________
+    % for object in form closure.
+    % contact = [1.5 1 225; -1.5 1 315; -1.5 -1 45; 1.5 -1 135];
+    % k = isFormClosure(contact);
+    % OUTPUT: The object is in form closure; corresponding k values.
     
     contact_coord = zeros(size(contact));
     contact_coord(:, 1:2) = contact(:, 1:2);
@@ -15,7 +27,7 @@ function k = isFormClosure(contact)
     wrench = horzcat(moment, normal(:, 1:2)).';    
     
     % check rank and calculate the linprog
-    if rank(wrench) == 3
+    if rank(wrench) >= 3
         f = ones(1, size(contact, 1));
         A = eye(size(contact, 1)).* -1;
         b = ones(1, size(contact, 1)) .* -1;
@@ -23,22 +35,14 @@ function k = isFormClosure(contact)
         beq = [0 0 0];
         [k, ~, exitflag] = linprog(f, A, b, Aeq, beq);
         
-        if exitflag == -2      
-            error("The object is not in form closure.");
-        else
+        if exitflag == 1 
             disp("The object is in form closure.");
+        else
+            error("The object is not in form closure.");
         end
+    else
+        fprintf("Rank of wrench matrix = %d\n", rank(wrench));
+        error("The object is not in form closure");  
     end
 end
 
-%{
-test
-
-% for object not in form closure.
-contact = [1.5 1 225; -1.5 1 315; -1.5 -1 45; 1.5 -1 154];
-k = isFormClosure(contact);
-
-% for object in form closure.
-contact = [1.5 1 225; -1.5 1 315; -1.5 -1 45; 1.5 -1 135];
-k = isFormClosure(contact);
-%}
